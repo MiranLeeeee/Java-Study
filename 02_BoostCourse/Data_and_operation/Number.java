b7ebf1815c5a773eb03c4dc6535488604c027ee1
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-04-08 16:52:56 +0900

    Number

---
+++ 02_BoostCourse/Data_and_operation/Number.java
@@ -0,0 +1,15 @@
+
+public class Number {
+	public static void main(String[] args) {
+		// Operator (사칙연산)
+		System.out.println(6+2); //8
+		System.out.println(6-2); //4
+		System.out.println(6*2); //12
+		System.out.println(6/2); //3
+		
+		//Math: 수학과 관련된 라이브러리
+		System.out.println(Math.PI);//3.141592653589793 (PI값을 가져옴)
+		System.out.println(Math.floor(Math.PI));//3.0 (내림: 소수점을 잘라버림)
+		System.out.println(Math.ceil(Math.PI));//4.0 (올림: 소수점을 올려버림)
+	}
+}
