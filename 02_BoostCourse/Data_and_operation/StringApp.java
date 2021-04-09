756ab5d63732430d9b671651986c9585930085f3
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-04-09 18:43:00 +0900

    StringApp

---
+++ 02_BoostCourse/Data_and_operation/StringApp.java
@@ -0,0 +1,15 @@
+
+public class StringApp {
+	public static void main(String[] args) {
+		//자바에서는 ''은 문자, ""은 문자(열)
+		System.out.println("Hello World"); //String: 문자열
+		System.out.println('H'); //Character
+		System.out.println("H");
+		
+		//escape
+		//줄바꿈
+		System.out.println("Hello \nWorld"); //new line
+		// Hello "World", ""은 문자의 시작과 끝을 의미함
+		System.out.println("Hello \"World\"");//""
+	}
+}
