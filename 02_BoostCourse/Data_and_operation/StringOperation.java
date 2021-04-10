bd4379461c62f93fb9d0a7f88d28bcf1a88ff224
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-04-10 17:42:00 +0900

    StringOperation

---
+++ 02_BoostCourse/Data_and_operation/StringOperation.java
@@ -0,0 +1,8 @@
+public class StringOperation {
+	public static void main(String[] args) {
+		//문자열의 길이
+		System.out.println("Hello World".length());
+		//문자열 대체 (바꾸기)
+		System.out.println("Hello, Kim ...Bye".replace("Kim", "Lee"));
+	}
+}
