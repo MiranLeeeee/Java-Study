3442f1cadb4aa828f580d8fb9356ff0644f18101
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-04-10 18:00:16 +0900

    Letter

---
+++ 02_BoostCourse/Data_and_operation/Letter.java
@@ -0,0 +1,11 @@
+
+public class Letter {
+	public static void main(String[] args) {
+		//변수이름을 통해 문자열에 대한 의미 파악 가능
+		String name = "Lee";
+		System.out.println("Hello,"+name+"....Kim...lee");
+		
+		double VAT = 10.0;
+		System.out.println(VAT);
+	}
+}
