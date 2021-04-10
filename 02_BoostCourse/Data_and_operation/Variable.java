c456fd161b382e8bb9aafe65085f6edfca7b92bd
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-04-10 17:52:08 +0900

    Variable

---
+++ 02_BoostCourse/Data_and_operation/Variable.java
@@ -0,0 +1,19 @@
+
+public class Variable {
+	public static void main(String[] args) {
+		//데이터타입을 명시해서 변수를 선언
+		
+		//Number -> integer (음수, 0, 양수)
+		int a = 1;
+		System.out.println(a);
+		
+		//Real number (실수) -> double
+		double b = 1.1;
+		System.out.println(b);
+		
+		//String (문자열)
+		String c = "Hello World";
+		System.out.println(c);
+		
+	}
+}
