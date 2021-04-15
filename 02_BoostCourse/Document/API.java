1d1a2904ba303ffd01f10193a8343c55b5151e30
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-04-15 20:41:25 +0900

    API.java (Math)

---
+++ 02_BoostCourse/Document/API.java
@@ -0,0 +1,10 @@
+import java.lang.Math; 
+//java.lang: 비슷한 성격을 가진 클래스의 모임을 패키지
+// Math: 수학과 관련한 클래스
+
+public class API {
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+	}
+
+}
