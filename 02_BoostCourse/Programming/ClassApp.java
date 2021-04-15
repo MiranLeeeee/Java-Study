f2909003e269b24e817e9657afcfca9bdefbaed8
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-04-16 00:07:40 +0900

    ClassApp.java

---
+++ 02_BoostCourse/Programming/ClassApp.java
@@ -0,0 +1,9 @@
+import java.lang.*;
+
+public class ClassApp {
+	public static void main(String[] args) {
+		System.out.println(Math.PI);
+		System.out.println(Math.floor(1.6));//소수점 내림: 1.0
+		System.out.println(Math.ceil(1.6));//소수점 올림 2.0
+	}
+}
