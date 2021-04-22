1c27f8b3da4ff2158e093b8bc3810db5e9b93a4c
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-04-22 16:47:42 +0900

    AccountingApp.java (Variable)

--- 02_BoostCourse/MyApp/AccountingApp.java
+++ 02_BoostCourse/MyApp/AccountingApp.java
@@ -2,15 +2,29 @@
 public class AccountingApp {
 	public static void main(String[] args) {
 		
-		System.out.println("Value of supply : "+ 10000.0); //공급가
-		System.out.println("VAT : "+ (10000.0*0.1)); //부가세
-		System.out.println("Total : "+ (10000.0 + 10000.0*0.1));//소비자가
+		double valueOfSupply = 10000.0; //공급가
+		double vatRate = 0.1;//부가세
+		double expenseRate = 0.3;//비용비율
 		
-		System.out.println("Expense : " + (10000.0*0.3));//비용
-		System.out.println("Income : " + (10000.0-10000.0*0.3));//순수입액
+		double vat = valueOfSupply*vatRate;
+		double total = valueOfSupply + vat;
+		double expense = valueOfSupply*expenseRate;
+		double income = valueOfSupply-expense;
 		
-		System.out.println("Dividend 1 : " + (10000.0-10000.0*0.3)*0.5);//첫번째 배당액
-		System.out.println("Dividend 2 : " + (10000.0-10000.0*0.3)*0.3);//두번째 배당액
-		System.out.println("Dividend 3 : " + (10000.0-10000.0*0.3)*0.2);
+		double dividend1 = income*0.5;
+		double dividend2 = income*0.3;
+		double dividend3 = income*0.2;
+		
+		System.out.println("Value of supply : "+ valueOfSupply); //공급가
+		System.out.println("VAT : "+ vat); //부가세
+		System.out.println("Total : "+ total);//소비자가
+			
+		
+		System.out.println("Expense : " + expense);//비용
+		System.out.println("Income : " + income);//순수입액
+		
+		System.out.println("Dividend 1 : " + dividend1);//첫번째 배당액
+		System.out.println("Dividend 2 : " + dividend2);//두번째 배당액
+		System.out.println("Dividend 3 : " + dividend3);//세번째 배당액
 	}
 }
