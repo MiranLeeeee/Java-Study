3569839e610e73e3e7848aeced164b7d39b98420
MiranLee <71805803+MiranLeeeee@users.noreply.github.com>
2021-04-27 00:05:16 +0900

    update AccountingClassApp.java

--- 02_BoostCourse/MyApp/AccountingClassApp.java
+++ 02_BoostCourse/MyApp/AccountingClassApp.java
@@ -1,8 +1,8 @@
 //클래스 생성 (연관)
 class Accounting {
-	public static double valueOfSupply = 10000.0; //공급가
-	public static double vatRate = 0.1;//부가세
-	public static double expenseRate = 0.3;//비용비율
+	public static double valueOfSupply;//공급가
+	public static double vatRate;//부가세
+	public static double expenseRate;//비용비율
 	
 	public static double getIncome() {
 		return valueOfSupply * getExpense();
@@ -20,37 +20,38 @@ class Accounting {
 		return valueOfSupply * vatRate;
 	}
 	
+	public static double getDividend1 () {
+		return getIncome() * 0.5;
+	}
+	
+	public static double getDividend2 () {
+		return getIncome() * 0.3;
+	}
+	
+	public static double getDividend3 () {
+		return getIncome() * 0.2;
+	}
+	
+	public static void print() {
+		System.out.println("Value of supply : "+ valueOfSupply); //공급가
+		System.out.println("VAT : "+ getVAT()); //부가세
+		System.out.println("Total : "+ getTotal());//소비자가		
+		System.out.println("Expense : " + getExpense());//비용
+		System.out.println("Income : " + getIncome());//순수입액		
+		System.out.println("Dividend 1 : " + getDividend1());//첫번째 배당액
+		System.out.println("Dividend 2 : " + getDividend2());//두번째 배당액
+		System.out.println("Dividend 3 : " + getDividend3());//세번째 배당액
+	}
 }
 
 public class AccountingClassApp {
-	
-	
 	public static void main(String[] args) {
-	
-		//메소드: 입력 값을 넣거나 전역변수로 지정하여 실행
-		double vat = Accounting.getVAT();
-		double total = Accounting.getTotal();
-		double expense = Accounting.getExpense();
-		double income = Accounting.getIncome();
+		Accounting.valueOfSupply = 10000.0;
+		Accounting.vatRate = 0.1;
+		Accounting.expenseRate = 0.3;
+				
+		Accounting.print();
 		
-		double dividend1 = income*0.5;
-		double dividend2 = income*0.3;
-		double dividend3 = income*0.2;
-		
-		System.out.println("Value of supply : "+ Accounting.valueOfSupply); //공급가
-		System.out.println("VAT : "+ vat); //부가세
-		System.out.println("Total : "+ total);//소비자가
-			
-		
-		System.out.println("Expense : " + expense);//비용
-		System.out.println("Income : " + income);//순수입액
-		
-		System.out.println("Dividend 1 : " + dividend1);//첫번째 배당액
-		System.out.println("Dividend 2 : " + dividend2);//두번째 배당액
-		System.out.println("Dividend 3 : " + dividend3);//세번째 배당액
-		
-		//another Variable, method
+		//another Variable, method......
 	}
-	
-	
 }
